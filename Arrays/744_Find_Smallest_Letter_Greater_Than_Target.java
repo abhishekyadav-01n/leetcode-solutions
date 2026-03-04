5fca81690471f697afdda25b2689be9bc9cd19ef
leetcode-solutions <abhasnp55@gmail.com>
2026-03-04 20:52:40 +0530

    Adding solution of 744_Find_Smallest_Letter_Greater_Than_Target

---
+++ Arrays/744_Find_Smallest_Letter_Greater_Than_Target.java
@@ -0,0 +1,20 @@
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int n = letters.length;
+        int min = Integer.MAX_VALUE;
+        int ch = 0;
+        for(int i = 0; i<n ; i++){
+            ch = letters[i] - target;
+            if(ch>0){
+                min = Math.min(ch,min);
+            }
+        }
+        if(ch<=0){
+            return letters[0];
+        }
+        int a = 0;
+        a = min + target;
+        char x = (char) a;
+        return x;
+    }
+}
