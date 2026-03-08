0ceea36fe1372c4cc377309bd1e69db284d5cc3d
leetcode-solutions <abhasnp55@gmail.com>
2026-03-08 22:58:09 +0530

    Added solution of Q2894

---
+++ Arrays/2894_Divisible_and_Non_divisible_Sums_Difference.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int differenceOfSums(int n, int m) {
+        int divSum = 0;
+        int ndivSum = 0;
+
+        for(int i = 1; i<=n ; i++){
+            if(i%m == 0){
+                divSum += i;
+            }
+            else{
+                ndivSum += i;
+            }
+        }
+        return ndivSum - divSum;
+    }
+}
