a4dc029ad49a159471a51e43e9425fe41504b412
leetcode-solutions <abhasnp55@gmail.com>
2026-03-20 19:46:17 +0530

    Adding solution of Q2535

--- Arrays/2535_Difference_Between_Element_Sum_and_Digit_Sum_of_an_Array.java
+++ Arrays/2535_Difference_Between_Element_Sum_and_Digit_Sum_of_an_Array.java
@@ -1,3 +1,17 @@
-class Solution_2535_Difference_Between_Element_Sum_and_Digit_Sum_of_an_Array {
-    
+class Solution {
+    public int differenceOfSum(int[] nums) {
+        int n = nums.length;
+        int sumEle = 0;
+        int sum = 0;
+        for(int i = 0; i<n ; i++){
+            int a = nums[i];
+            sumEle += nums[i];
+            while(a>0){
+                int rem = a % 10;
+                sum += rem;
+                a = a/10;
+            }
+        }
+        return Math.abs(sumEle - sum);
+    }
 }
