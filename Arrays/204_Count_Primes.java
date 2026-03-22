97cc3d7d6830006f1e5e0307c33b7a394a9967cb
leetcode-solutions <abhasnp55@gmail.com>
2026-03-22 20:02:05 +0530

    Added solution of Q204

---
+++ Arrays/204_Count_Primes.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public int isPrimes(int n) {
+        if(n <= 2) return 0;
+
+        boolean[] isComposite = new boolean[n];
+        List<Integer> primes = new ArrayList<>();
+
+        for(int i = 2; i < n; i++){
+            if(!isComposite[i]){
+                primes.add(i);
+            }
+
+            for(int p : primes){
+                if(i * p >= n) break;
+
+                isComposite[i * p] = true;
+
+                if(i % p == 0) break;
+            }
+        }
+
+        return primes.size();
+    }
+    public int countPrimes(int n) {
+        return isPrimes(n);
+    }
+}
