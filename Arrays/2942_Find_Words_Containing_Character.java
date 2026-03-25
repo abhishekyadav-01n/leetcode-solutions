70c6257fda2f79d732c8688a96fbdd8575d5513c
Abhishek Yadav <abhasnp55@gmail.com>
2026-03-25 11:34:01 +0530

    Add method to find words containing a specific characters

--- Arrays/2942_Find_Words_Containing_Character.java
+++ Arrays/2942_Find_Words_Containing_Character.java
@@ -1,22 +1,9 @@
 class Solution {
-    public int findGCD(int[] nums) {
-        int n = nums.length;
-
-        int max = Integer.MIN_VALUE;
-        int min = Integer.MAX_VALUE;
-
-        for(int i = 0; i<n ; i++){
-            max = Math.max(max,nums[i]);
-            min = Math.min(min,nums[i]);
+    public List<Integer> findWordsContaining(String[] words, char x) {
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0; i<words.length; i++){
+            if(words[i].contains(String.valueOf(x))) list.add(i);
         }
-        while(min>0){
-            int x = max % min;
-            if(x == 0){
-                break;
-            }
-            max = min;
-            min = x;
-        }
-        return min;
+        return list;
     }
 }
