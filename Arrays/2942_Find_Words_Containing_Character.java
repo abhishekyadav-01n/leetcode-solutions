fc44d5e848cd040158dbc551137ea70d134b5b7f
Abhishek Yadav <abhasnp55@gmail.com>
2026-03-25 11:33:20 +0530

    Add method to find GCD of an array of integers
    Implement findGCD method to calculate the GCD of an array.

--- Arrays/2942_Find_Words_Containing_Character.java
+++ Arrays/2942_Find_Words_Containing_Character.java
@@ -1,2 +1,22 @@
-// original file content here
-// Assume we are preserving the original content 
+class Solution {
+    public int findGCD(int[] nums) {
+        int n = nums.length;
+
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+
+        for(int i = 0; i<n ; i++){
+            max = Math.max(max,nums[i]);
+            min = Math.min(min,nums[i]);
+        }
+        while(min>0){
+            int x = max % min;
+            if(x == 0){
+                break;
+            }
+            max = min;
+            min = x;
+        }
+        return min;
+    }
+}
