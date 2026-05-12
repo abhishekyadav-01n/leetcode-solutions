9d5b93eddb7a9f202956c7e61355e0c2d83b6096
Abhishek Yadav <abhasnp55@gmail.com>
2026-05-12 20:18:49 +0530

    Added solution of 3890

---
+++ Arrays/3890_Integers_With_Multiple_Sum_of_Two_Cubes.java
@@ -0,0 +1,30 @@
+class Solution {
+    public List<Integer> findGoodIntegers(int n) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        int limit = (int) Math.cbrt(n);
+
+        for (int a = 1; a <= limit; a++) {
+            int a3 = a * a * a;
+
+            for (int b = a; b <= limit; b++) {
+                int sum = a3 + b * b * b;
+
+                if (sum > n) break;
+
+                map.put(sum, map.getOrDefault(sum, 0) + 1);
+            }
+        }
+
+        List<Integer> result = new ArrayList<>();
+
+        for (int key : map.keySet()) {
+            if (map.get(key) >= 2) {
+                result.add(key);
+            }
+        }
+
+        Collections.sort(result);
+        return result;
+    }
+}
