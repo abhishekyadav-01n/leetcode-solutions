e0ee6ab6d67b2f1de709e3ac641ee6f3fbf12619
Abhishek Yadav <abhasnp55@gmail.com>
2026-05-20 17:10:10 +0530

    Added solution of 162

---
+++ Arrays/162_Find_Peak_Element.java
@@ -0,0 +1,17 @@
+class Solution { //Binary Search
+    public int findPeakElement(int[] nums) {
+        int low = 0, high = nums.length - 1;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] > nums[mid + 1]) {
+                high = mid;  // peak on left
+            } else {
+                low = mid + 1;  // peak on right
+            }
+        }
+
+        return low;
+    }
+}
