3f74abc395190890b5139e2a88ff017e3f09f522
Abhishek Yadav <abhasnp55@gmail.com>
2026-06-01 21:27:54 +0530

    Added solution of 3898

---
+++ Arrays/3898_Find_the_Degree_of_Each_Vertex.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] findDegrees(int[][] matrix) {
+        int n = matrix.length;
+
+        int nums[] = new int[n];
+        
+        int x = 0;
+        for(int i = 0; i<n ; i++){
+            int count = 0;
+            for(int j = 0; j<n ; j++){
+                if(matrix[i][j] == 1) count++;
+            }
+            nums[x++] = count;
+        }
+        return nums;
+    }
+}
