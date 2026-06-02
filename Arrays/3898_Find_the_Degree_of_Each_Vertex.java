cb324149df561432285e79399f123b445614e768
Abhishek Yadav <abhasnp55@gmail.com>
2026-06-02 22:30:22 +0530

    Added solution of 3898
    Added a loop to count the degree of each vertex in the matrix.

--- Arrays/3898_Find_the_Degree_of_Each_Vertex.java
+++ Arrays/3898_Find_the_Degree_of_Each_Vertex.java
@@ -5,6 +5,7 @@ class Solution {
         int nums[] = new int[n];
         
         int x = 0;
+        
         for(int i = 0; i<n ; i++){
             int count = 0;
             for(int j = 0; j<n ; j++){
