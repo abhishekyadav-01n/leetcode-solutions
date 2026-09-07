9dae3db3bc2b19337a94705891db2cf2ee9a6682
Abhishek Yadav <abhasnp55@gmail.com>
2026-09-07 17:23:24 +0530

    Time: 30 ms (85.21%), Space: 69.4 MB (54.18%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.java
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.java
@@ -1,14 +1,13 @@
 class Solution {
-    private int findJobs(int i , int jobs[][]){
-        int target = jobs[i][1];
-
+    private int findJobs(int i, int jobs[][]){
         int left = i+1;
+        int target = jobs[i][1];
         int right = jobs.length;
 
         while(left < right){
             int mid = left + (right - left) / 2;
 
-            if( jobs[mid][0] >= target ){
+            if(jobs[mid][0] >= target){
                 right = mid;
             }
             else{
@@ -19,8 +18,9 @@ class Solution {
     }
     public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
         int n = profit.length;
-
         int jobs[][] = new int[n][3];
+        int dp[] = new int[n+1];
+
         for(int i = 0; i<n ; i++){
             jobs[i][0] = startTime[i];
             jobs[i][1] = endTime[i];
@@ -28,13 +28,11 @@ class Solution {
         }
         Arrays.sort(jobs , (a,b) -> (a[0] - b[0]));
 
-        int dp[] = new int[n+1];
-
         for(int i = n-1; i>=0 ; i--){
-            int next = findJobs(i , jobs);
-            int taken = jobs[i][2] + dp[next];
-            int skip = dp[i + 1];
-            dp[i] = Math.max(taken , skip);
+            int next = findJobs(i ,jobs);
+            int taken = dp[next] + jobs[i][2];
+            int skip = dp[i+1];
+            dp[i] = Math.max(skip , taken);
         }
         return dp[0];
     }
