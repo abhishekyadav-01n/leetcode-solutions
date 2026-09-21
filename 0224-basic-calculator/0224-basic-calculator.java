51a7d56ffa31b40e59cd542f4e132780b7525a1a
Abhishek Yadav <abhasnp55@gmail.com>
2026-09-21 14:07:58 +0530

    Time: 12 ms (33.96%), Space: 45.9 MB (97.97%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.java
+++ 0224-basic-calculator/0224-basic-calculator.java
@@ -1,10 +1,9 @@
 class Solution {
     public int calculate(String s) {
         Stack<Integer> stack = new Stack<>();
-
         int result = 0;
-        int sign = 1;
         int num = 0;
+        int sign = 1;
 
         for(int i = 0; i<s.length() ; i++){
             char ch = s.charAt(i);
@@ -13,26 +12,25 @@ class Solution {
                 num = num * 10 + ch - '0';
             }
             else if(ch == '+'){
-                result += sign * num;
+                result += num * sign;
                 num = 0;
                 sign = 1;
             }
             else if(ch == '-'){
-                result += sign * num;
+                result += num * sign;
                 num = 0;
                 sign = -1;
             }
             else if(ch == '('){
                 stack.push(result);
                 stack.push(sign);
-
                 result = 0;
                 sign = 1;
             }
             else if(ch == ')'){
                 result += sign * num;
-                num = 0;
 
+                num = 0;
                 int prevSign = stack.pop();
                 int prevResult = stack.pop();
 
