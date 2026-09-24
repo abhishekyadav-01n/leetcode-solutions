19e73e8af96d732266668ca15158d54643a17b76
Abhishek Yadav <abhasnp55@gmail.com>
2026-09-24 23:01:31 +0530

    Time: 0 ms (100%), Space: 43.1 MB (29.36%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
@@ -10,23 +10,33 @@
  */
 class Solution {
     public ListNode reverseBetween(ListNode head, int left, int right) {
-        if(head == null || head.next == null || right == left) return head;
+        if(head == null || head.next == null || left == right){
+            return head;
+        }
         ListNode dummy = new ListNode(0);
         dummy.next = head;
 
-        ListNode prev = dummy;
+        ListNode leftNode = head;
+        ListNode rightNode = head;
+        ListNode prevLeftNode = dummy;
         for(int i = 1; i<left ; i++){
-            prev = prev.next;
+            leftNode = leftNode.next;
+            prevLeftNode = prevLeftNode.next;
+        }
+        for(int i = 1; i<right ; i++){
+            rightNode = rightNode.next;
         }
 
-        ListNode curr = prev.next;
-        for(int i = 0; i<right-left ; i++){
-            ListNode newNode = curr.next;
-            curr.next = newNode.next;
-            newNode.next = prev.next;
-            prev.next = newNode;
+        ListNode stop = rightNode.next;
+        ListNode next = stop;
+        while(leftNode != stop){
+            ListNode temp = leftNode.next;
+            leftNode.next = next;
+            next = leftNode;
+            leftNode = temp;
         }
 
+        prevLeftNode.next = rightNode;
         return dummy.next;
     }
 }
